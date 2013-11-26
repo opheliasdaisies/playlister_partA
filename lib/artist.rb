1b2a1fa57a4c4b4997dc689362f7abb1203b095a
Sara Gorecki <sgorecki@gmail.com>
2013-11-26 16:51:22 -0500

    add initialize, reset, all, and count methods for artist

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,27 @@
 class Artist
-  
-  def initialize
+  attr_accessor :name, :songs
 
+  ARTISTS = []
+  @@artist_count = 0
+
+  def initialize
+    @name
+    @songs
+    @@artist_count += 1
+    ARTISTS << self
   end
   
+  def self.reset_artists
+    ARTISTS.clear
+    @@artist_count = 0
+  end
+
+  def self.count
+    @@artist_count
+  end
+
+  def self.all
+    ARTISTS
+  end
+
 end
