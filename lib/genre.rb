b278ab2beaf3a8669ff2b9bf08a4b0ba929582f2
Sara Gorecki <sgorecki@gmail.com>
2013-11-26 19:26:57 -0500

    add genre array constant

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,8 +1,20 @@
 class Genre
-  attr_accessor :name
+  attr_accessor :name, :songs
+
+  GENRES = []
 
   def initialize
     @name
+    @songs = []
+    GENRES << self
+  end
+
+  def self.all
+    GENRES
+  end
+
+  def self.reset_genres
+    GENRES.clear
   end
 
 end
